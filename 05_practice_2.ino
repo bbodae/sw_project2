b9caa7d3ce9cfe48cf0b99d516be9c26c443e0f9
bbodae <59393317+bbodae@users.noreply.github.com>
2023-09-19 11:29:37 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,33 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+
+  delay(1000);
+  
+  Serial.println(++count);
+  for (int i = 0; i<10; ) {
+    i = i + 1 ;
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle);
+    delay (100);
+  }
+  while (1) {
+    digitalWrite(PIN_LED, 1);
+  }
+
+}
+
+unsigned int toggle_state(unsigned int toggle) {
+  toggle = ++toggle ;
+  return toggle%2 ;
+}
